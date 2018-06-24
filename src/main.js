f3cb0eed16625d630d5caccdb68ecb3081843978
Martin Feineis <martinfeineis@gmx.de>
2018-06-24 13:12:11 +0200

    Exposing `IntlMessageFormat` and properly loading locale configs from CDN (#2)

--- src/main.js
+++ src/main.js
@@ -1,12 +1,22 @@
+import IntlMessageFormat from "intl-messageformat";
+
 import { configureIntlElements } from "./core";
 import { define as defineSpan } from "./elements/span";
 
-const setDocumentLang = lang => (
-    document.querySelector("html").setAttribute("lang", lang)
-);
+const setDocumentLang = lang => {
+    document.querySelector("html").setAttribute("lang", lang);
+
+    const localeInclude = document.createElement("script");
+    localeInclude.setAttribute(`data-intl-locale-include-${lang}`, "");
+    localeInclude.src = `https://cdnjs.cloudflare.com/ajax/libs/intl-messageformat/2.2.0/locale-data/${lang}.js`;
+    document.querySelector("head").appendChild(localeInclude);
+};
 
 const IntlElements = configureIntlElements(setDocumentLang);
 
 defineSpan(IntlElements, window.customElements);
+
+// We need to expose the constructor for dynamic locale data loading
+window.IntlMessageFormat = IntlMessageFormat;
 
 export default IntlElements;
