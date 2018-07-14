527f0b9fa4ac1de25b5673fd92bb85058aa7a51b
Martin Feineis <martinfeineis@gmx.de>
2018-07-14 17:10:07 +0200

    Moved translation related stuff into `Translation` module (#2)

--- demo/src/Main.elm
+++ demo/src/Main.elm
@@ -3,9 +3,9 @@ module Main exposing (main)
 import Browser
 import Html exposing (Html)
 import Html.Attributes as Attr
+import Translation exposing (LangKey(..), t)
 import Intl
 import Json.Decode as Decode exposing (Value)
-import Json.Encode as Encode
 
 
 main : Program Value Model Msg
@@ -25,62 +25,6 @@ type alias Model =
 
 type Msg
     = Noop
-
-
-type LangKey
-    = SomeButton
-    | SomeOtherKey Float
-    | SomePlaceholder
-
-
-
--- FIXME: The formats should really be strongly typed!
-
-
-attachDollarFormat : Intl.Spec -> Intl.Spec
-attachDollarFormat =
-    Intl.mapFormats
-        (Encode.object
-            [ ( "number"
-              , Encode.object
-                    [ ( "USD"
-                      , Encode.object
-                            [ ( "style", Encode.string "currency" )
-                            , ( "currency", Encode.string "USD" )
-                            ]
-                      )
-                    ]
-              )
-            ]
-        )
-
-
-attachPrice : Float -> Intl.Spec -> Intl.Spec
-attachPrice price =
-    -- FIXME: We get runtime errors when the values don't fit or
-    --        if they are omitted :-/
-    Intl.mapValues
-        (Encode.object
-            [ ( "price", Encode.float price )
-            ]
-        )
-
-
-t : LangKey -> Intl.Spec
-t key =
-    case key of
-        SomeButton ->
-            Intl.spec "some.otherKey"
-                |> attachPrice 0.02
-                |> attachDollarFormat
-
-        SomeOtherKey price ->
-            Intl.spec "some.otherKey"
-                |> attachPrice price
-                |> attachDollarFormat
-
-        SomePlaceholder ->
-            Intl.spec "some.placeholder"
 
 
 showPrice : { ctx | somePrice : Float } -> Html msg -> Html msg
